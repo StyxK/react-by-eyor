8a68be18de0148ac4e16bfe0e991f51c632b4b29
BounceGem <mildsadz@gmail.com>
2020-01-30 01:59:19 +0700

    Test Branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [number, setNum] = useState(0)
+  useEffect(() => {
+    setNum(number + 3)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
+        <p>number : {number}</p>
+        <button onClick={() => { setNum(number + 1) }}> Increse </button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <App2 number={number} />
       </header>
     </div>
   );
+}
+
+class App2 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      fromApi: {}
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch("https://api.github.com/repos/StyxK/React-")
+    const data = await response.json()
+    this.setState({
+      fromApi: data
+    })
+  }
+
+  render() {
+    let number = 1
+    const incNum = () => {
+      number = number + 1
+      this.setState({
+        count: ++this.state.count
+      })
+    }
+    return (
+      <div>
+        <button onClick={() => { incNum() }}> Increse </button>
+        <p> not a state : {number} </p>
+        <p> a state : {this.state.count} </p>
+        <p> a props : {this.props.number} </p>
+        {console.log(this.state.fromApi)}
+      </div>
+    )
+  }
 }
 
 export default App;
